9999b72209740df253a453c0526292a706e07c05
Jeon <gino9940@gmail.com>
2023-05-27 21:50:44 +0900

    feat: add screens

---
+++ src/screens/achievement/AchievementScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AchievementScreen = () => {
+  return (
+    <View>
+      <Text>AchievementScreen</Text>
+    </View>
+  );
+};
+
+export default AchievementScreen;
---
+++ src/screens/exercise/SelectExerciseScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SelectExerciseScreen = () => {
+  return (
+    <View>
+      <Text>SelectExerciseScreen</Text>
+    </View>
+  );
+};
+
+export default SelectExerciseScreen;
