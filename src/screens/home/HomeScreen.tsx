ec2fa0f2dccf4b654046878ef1d6c5c28fcefa7b
Jeon <gino9940@gmail.com>
2023-06-04 16:11:30 +0900

    Revert "Revert "fix: remove type from async-storage""
    This reverts commit 5eecc09c82f138ed58d0b2b3fd636f9c2cb60835.

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-/**
- * ? Local Imports
- */
-/**
- * ? Shared Imports
- */
+import Logo from "@shared-components/Logo";
+import { getData, KEY } from "@utils";
+import { Text } from "react-native";
+import User from "../../shared/types/User";
 
 const HomeScreen = () => {
-  return <SafeAreaView />;
+  const [user, setUser] = useState<User>();
+
+  useLayoutEffect(() => {
+    const initData = async () => {
+      const storedUser = (await getData(KEY.USER)) as User;
+
+      setUser(storedUser);
+    };
+
+    initData();
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <Logo />
+      <Text>{user?.name}</Text>
+    </SafeAreaView>
+  );
 };
 
 export default HomeScreen;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,26 +4,14 @@ const KEY = {
   USER: "@user",
 } as const;
 
-const TYPE = {
-  [KEY.USER]: {
-    name: "",
-    age: "",
-    gender: "",
-    height: "",
-    weight: "",
-  },
-} as const;
-
 const storeData = async (key: typeof KEY[keyof typeof KEY], value: unknown) => {
   const jsonValue = JSON.stringify(value);
   await AsyncStorage.setItem(key, jsonValue);
 };
 
-const getData = async (
-  key: typeof KEY[keyof typeof KEY],
-): Promise<typeof TYPE[typeof key]> => {
+const getData = async (key: typeof KEY[keyof typeof KEY]) => {
   const jsonValue = await AsyncStorage.getItem(key);
   return jsonValue != null ? JSON.parse(jsonValue) : null;
 };
 
-export { KEY, TYPE, storeData, getData };
+export { KEY, storeData, getData };
